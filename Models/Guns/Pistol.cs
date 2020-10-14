30f14fba81a0dbea9d38737cce14e5ef6e4f656e
opetranov <opetranov@gmail.com>
2020-10-14 22:07:32 +0300

    Models

---
+++ Models/Guns/Pistol.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CounterStrike.Models.Guns
+{
+    public class Pistol : Gun
+    {
+        private const int MAX_BULLETS_PER_SHOT = 1;
+        public Pistol(string name, int bulletsCount) 
+            : base(name, bulletsCount)
+        {
+        }
+
+        public override int Fire()
+        {
+            int bulletsFired = 0;
+
+            if (BulletsCount > 0)
+            {
+                bulletsFired += MAX_BULLETS_PER_SHOT;
+                this.BulletsCount-= MAX_BULLETS_PER_SHOT;
+            }
+
+            return bulletsFired;
+        }
+    }
+}
